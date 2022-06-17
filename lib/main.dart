39a5ce190da6e88e13bf8815f7173165c38545f5
jpohhhh <jpohhhh@gmail.com>
2022-06-17 12:16:57 -0400

    Update title bar name to Giverny

--- lib/main.dart
+++ lib/main.dart
@@ -12,9 +12,8 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Giverny',
       theme: ThemeData(
-
       ),
       home: Scaffold(
         appBar: AppBar(
